ba4d01cabedf0e765c80232f3b471a2326df53f7
asunson <aaronsunson@gmail.com>
2018-12-18 11:48:04 -0500

    Modified path creation to be portable between mac and windows

--- main.py
+++ main.py
@@ -13,28 +13,28 @@ experiments = file.readlines()
 
 # find all culture types used within each experiment
 for e in experiments:
-    culture_types = [e + d + "/" for d in os.listdir(e) if os.path.isdir(os.path.join(e, d))]
+    culture_types = [os.path.join(e, d) for d in os.listdir(e) if os.path.isdir(os.path.join(e, d))]
     
     # iterate through experimental conditions within each culture type
     for path in culture_types:
-        experiment_conditions = [path + d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+        experiment_conditions = [os.path.join(path, d) for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
         # create excel sheet to store quantification results for a given culture and corresponding conditions
-        writer = pd.ExcelWriter(path + "Quantification Results.xlsx")
+        writer = pd.ExcelWriter(os.path.join(path, "Quantification Results.xlsx"))
         
         # each condition contains images to be quantified. Scan through each and run quantifying script
         for experiment in experiment_conditions:
             # create a subdirectory to store annotated cell count images if one does not already exist
-            directory = experiment + "/Cell Counts"
+            directory = os.path.join(experiment, "Cell Counts")
             if not os.path.exists(directory):
                 os.makedirs(directory)
 
             # collect list of all tif files within each folder
-            imgnames = [i for i in glob.glob(experiment + "/*.tif")]
+            imgnames = [i for i in glob.glob(os.path.join(experiment, "*.tif"))]
             imgnames.sort()
 
             print("******")
-            print("Now quantifying: ", experiment.split('/')[-1])
+            print("Now quantifying: ", experiment.split(os.sep)[-1])
             print("******")
 
             # quantify cell counts and thresholds 
@@ -43,8 +43,8 @@ for e in experiments:
             # output final cell counts and thresholds corresponding to each image into excel sheet
             d = {"File Name": raw_imgnames, "Cell Count": final_cell_counts, "Threshold": thresholds}
             df = pd.DataFrame(data = d)
-            df.to_excel(writer, experiment.split('/')[-1])
+            df.to_excel(writer, experiment.split(os.sep)[-1])
     
         writer.save()
-        print("Done with Culture: ", path.split('/')[-2])
-    print("Done with Experiment: ", e.split('/')[-2])
+        print("Done with Culture: ", path.split(os.sep)[-2])
+    print("Done with Experiment: ", e.split(os.sep)[-2])
